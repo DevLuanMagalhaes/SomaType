f0bda19f5a6144f4e51a94446789fc05a918255f
DevLuanMagalhaes <l.g.magalhaes01@gmail.com>
2022-10-18 09:56:26 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/SSMA/SSMA/models/TwoEmplImmunizationCtrl.java
@@ -0,0 +1,59 @@
+package SSMA.SSMA.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TWO_EMPL_IMMUNIZATION_CTRL")
+public class TwoEmplImmunizationCtrl implements TwoEntity{
+
+    @Id
+    @Column(name="EMPL_IMMUNIZATION_CTRL_ID", nullable = false)
+    //@GeneratedValue(generator="seqEmplImmunizationCtrlId")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long emplImmunizationCtrlId;
+    @Column(name="ESTABID")
+    private String estabid;
+    @Column(name="EMPLID")
+    private String emplid;
+    @Column(name="EMPL_RCD")
+    private Long emplRcd;
+    @Column(name="VACCINE_DATE")
+    private Timestamp vaccineDate;
+    @Column(name="VACCINE_REFUSAL")
+    private String vaccineRefusal;
+    @Column(name="VACCINE_REFUSAL_JUSTIFIC")
+    private String vaccineRefusalJustific;
+    @Column(name="RETURN_DATE")
+    private Timestamp returnDate;
+    @Column(name="LABORATORY")
+    private String laboratory;
+    @Column(name="LOTE_NUMBER")
+    private String loteNumber;
+    @Column(name="NOTE")
+    private String note;
+    @Column(name="CREATED_BY", nullable = false)
+    private Long createdBy;
+    @Column(name="CREATION_DATE", nullable = false)
+    private Timestamp creationDate;
+    @Column(name="LAST_UPDATED_BY", nullable = false)
+    private Long lastUpdatedBy;
+    @Column(name="LAST_UPDATE_DATE", nullable = false)
+    private Timestamp lastUpdateDate;
+
+    @OneToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="VACCINE_ID", referencedColumnName="VACCINE_ID")
+    private TwoVaccine vaccineCollection;
+
+    @OneToOne(fetch= FetchType.EAGER)
+    @JoinColumn(name="VACCINE_DOSE_ID", referencedColumnName="VACCINE_DOSE_ID")
+    private TwoVaccineDose vaccineDoseCollection;
+
+}
---
+++ src/main/java/SSMA/SSMA/repository/RepositoryEmplImmunizationCtrl.java
@@ -0,0 +1,43 @@
+package SSMA.SSMA.repository;
+
+import SSMA.SSMA.models.TwoEmplImmunizationCtrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface RepositoryEmplImmunizationCtrl extends JpaRepository <TwoEmplImmunizationCtrl, Long> {
+
+
+    @Query(
+            value ="Select i from TwoEmplImmunizationCtrl i " +
+                    "where i.estabid = :estabid " +
+                    "  and i.emplid = :emplid " +
+                    "  and i.emplRcd = :emplRcd " +
+                    "order by i.vaccineDate "
+    )
+    public TwoEmplImmunizationCtrl findEmplImmunizationCtrl (@Param("estabid")String estabid,
+                                                             @Param("emplid")String emplid,
+                                                             @Param("emplRcd")Long emplRcd);
+
+
+
+    @Query(
+            value = "Select i " +
+                    "from TwoEmplImmunizationCtrl i " +
+                    "where i.estabid = :estabid " +
+                    "  and i.emplid = :emplid  " +
+                    "  and i.emplRcd = :emplRcd "+
+                    "  and i.vaccineId = :vaccineId "+
+                    "  and i.vaccineDate = (Select max(i2.vaccineDate) " +
+                    "                        from TwoEmplImmunizationCtrl i2 "+
+                    "                        where i2.estabid = i.estabid " +
+                    "                          and i2.emplid = i.emplid " +
+                    "                          and i2.emplRcd = i.emplRcd "+
+                    "                          and i2.vaccine_id = i.vaccine_id) " +
+                    "  and rownum = 1 "    ,nativeQuery = true
+   )
+    public TwoEmplImmunizationCtrl findMaxVaccineDate (@Param("estabid")String estabid,
+                                                       @Param("emplid")String emplid,
+                                                       @Param("emplRcd")Long emplRcd,
+                                                      @Param("vaccineId")Long vaccineId);
+}
